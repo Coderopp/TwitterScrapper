7db1a06b89eca618b280eb7106294f1115608ef1
Coderopp <96039946+Coderopp@users.noreply.github.com>
2023-05-29 00:40:46 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,12 @@
+import twint
+#configuration
+config = twint.Config()
+config.Search = "bitcoin"
+config.Lang = "en"
+config.Limit = 100
+config.Since = "2019–04–29"
+config.Until = "2020–04–29"
+config.Store_json = True
+config.Output = "custom_out.json"
+#running search
+twint.run.Search(config)
